81fdf78024066ae3b0442e7687d3c37d51cc8e80
nktrtmv <nvartemov@edu.hse.ru>
2023-01-08 12:37:17 +0300

    Обновлен класс Graph

--- hw/src/FileGraph.java
+++ hw/src/FileGraph.java
@@ -6,15 +6,48 @@ public class FileGraph {
     private ArrayList<Path> files;
     private int[][] adjacencyMatrix;
 
-    public void sort(){
-
+    public boolean sort() {
+        if (isValid()){
+            bfsSort();
+            return true;
+        }
+        return false;
     }
 
-    public FileGraph(ArrayList<Path> folderFiles){
+    private void bfsSort(){
+        // топологическая сортировка графа
+    }
+
+    public FileGraph(ArrayList<Path> folderFiles) {
         this.files = folderFiles;
+        nodes = new ArrayList<>();
+        for (Path file : files) {
+            nodes.add(new Node(file, files));
+        }
+        adjacencyMatrix = new int[nodes.size()][nodes.size()];
+        for (Node node: nodes) {
+            for (Path dependency: node.getDependencies()) {
+                adjacencyMatrix[files.indexOf(dependency)][files.indexOf(node.getPath())] = 1;
+            }
+        }
     }
 
-    public ArrayList<Path> getFiles(){
-        return  files;
+    private boolean isValid(){
+        for (int i = 0; i < adjacencyMatrix.length; ++i) {
+            for (int j = 0; j < adjacencyMatrix[i].length; ++j) {
+                if (i != j && adjacencyMatrix[i][j] == adjacencyMatrix[j][i] && adjacencyMatrix[i][j] != 0) {
+                    System.out.println("Обнаружены файлы с ссылками друг на друга.");
+                    return false;
+                }
+            }
+        }
+
+        // проверка на циклы
+
+        return true;
+    }
+
+    public ArrayList<Path> getFiles() {
+        return files;
     }
 }
