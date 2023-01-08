6eda8044034aad52afd6907dd8d33909a21b6045
nktrtmv <nvartemov@edu.hse.ru>
2023-01-08 11:25:49 +0300

    Обновлен Main

--- hw/src/Main.java
+++ hw/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,27 +8,36 @@ import java.util.Scanner;
 
 
 public class Main {
-    private static String folderPath = "test";
-    private static String answerPath = "output/ans.txt";
+    private static String folder = "test";
+    private static String result = "output/ans.txt";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int option = 0;
         do {
             menu();
-            while (option != 1 && option != 2){
-                try{
+            while (option != 1 && option != 2) {
+                try {
                     option = in.nextInt();
-                }catch (InputMismatchException ex){
+                } catch (InputMismatchException ex) {
                     System.out.println("Неккоректный ввод. Введите '1' или '2'.");
                 }
             }
-            if (option == 1 || (getFolderDirectory(in) && getFileDirectory(in))){
-                FileSorter sorter = new FileSorter(folderPath, answerPath);
-                if (!sorter.sortAndConcatenate()){
-                    System.out.println("Завершение программы.");
+            if (option == 1 || (getFolderDirectory(in) && getFileDirectory(in))) {
+                try {
+                    FileSorter sorter = new FileSorter(folder, result);
+                    if (sorter.sort()) {
+                        sorter.mergeFiles();
+                    } else {
+                        System.out.println("Ошибка при сортировки файлов.");
+                    }
+                } catch (IOException ex) {
+                    System.out.println("Ошибка при получении файлов.");
+                } catch (RuntimeException ex) {
+                    System.out.println("Ошибка при конкатенации отсортированных файлов.");
                 }
             }
+            System.out.println("Завершение программы.");
             System.out.println("Если вы желаете воспользоваться программой снова, введите 'y'.\n" +
                     "Для завершения работы программы нажмите на любую кнопку.");
         } while (Objects.equals(in.next(), "y"));
@@ -36,9 +46,9 @@ public class Main {
 
     private static boolean getFolderDirectory(Scanner in) {
         System.out.print("Введите директорию рабочей папки: ");
-        folderPath = in.next();
-        Path directory = Paths.get(folderPath);
-        if (!Files.exists(directory)){
+        folder = in.next();
+        Path directory = Paths.get(folder);
+        if (!Files.exists(directory)) {
             System.out.println("Введена неккоректная директория или программа не имеет к ней доступа.");
             return false;
         }
@@ -47,9 +57,9 @@ public class Main {
 
     private static boolean getFileDirectory(Scanner in) {
         System.out.print("Введите путь до итогового файла: ");
-        answerPath = in.next();
-        Path directory = Paths.get(answerPath);
-        if (!Files.exists(directory)){
+        result = in.next();
+        Path directory = Paths.get(result);
+        if (!Files.exists(directory)) {
             System.out.println("Введена неккоректная директория или программа не имеет к ней доступа.");
             return false;
         }
