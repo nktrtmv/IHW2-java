c8d2bb8705e2f942c673046aea00a826b5081eb0
nktrtmv <nvartemov@edu.hse.ru>
2023-01-08 21:07:50 +0300

    Граф файлов
    В текущем коммите изменил сортировку файлов на топологическую с сортировки обходом в ширину. Изменил проверку на наличие циклов с обхода в ширину в цикле на счетчик в топологической.

--- hw/src/FileGraph.java
+++ hw/src/FileGraph.java
@@ -4,43 +4,47 @@ import java.util.*;
 public class FileGraph {
     private ArrayList<Path> files;
     private final HashMap<Path, ArrayList<Node>> adjacencyList;
-    private final ArrayList<Path> result;
 
     public boolean sort() {
-        if (!hasLoop()) {
-            sortFiles();
-            return true;
-        }
-        return false;
-    }
-
-    private void sortFiles() {
-        HashSet<Path> visited = new HashSet<>();
+        HashMap<Path, Integer> inDegree = new HashMap<>();
         for (Path path : adjacencyList.keySet()) {
-            if (!visited.contains(path)) {
-                visited.addAll(bfs(path));
-            }
+            inDegree.put(path, (new Node(path, files).getDependencies()).size());
         }
-        files = result;
-    }
 
-    private HashSet<Path> bfs(Path path) {
-        HashSet<Path> visited = new HashSet<>();
-        ArrayDeque<Path> queue = new ArrayDeque<>();
-        queue.addLast(path);
-        while (!queue.isEmpty()){
-            result.add(queue.getFirst());
-            visited.add(queue.getFirst());
-            for (Node node : adjacencyList.get(queue.pop())){
-                queue.addLast(node.getPath());
+        Queue<Path> queue = new LinkedList<>();
+        for (Path path : inDegree.keySet()) {
+            if (inDegree.get(path) == 0) {
+                queue.add(path);
             }
         }
-        return visited;
+
+        int countOfVisited = 0;
+
+        ArrayList<Path> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Path path = queue.poll();
+            result.add(path);
+
+            for (Node node : adjacencyList.get(path)) {
+                inDegree.put(node.getPath(),inDegree.get(node.getPath()) - 1);
+                if (inDegree.get(node.getPath()) == 0){
+                    queue.add(node.getPath());
+                }
+            }
+            countOfVisited++;
+        }
+
+        files = result;
+        if (countOfVisited != adjacencyList.size()) {
+            System.out.println("Обнаружен цикл.");
+            return false;
+        }
+
+        return true;
     }
 
     public FileGraph(ArrayList<Path> folderFiles) {
         this.files = folderFiles;
-        result = new ArrayList<>();
         ArrayList<Node> nodes = new ArrayList<>();
         for (Path file : files) {
             nodes.add(new Node(file, files));
@@ -54,29 +58,6 @@ public class FileGraph {
                 adjacencyList.get(dependency).add(node);
             }
         }
-    }
-
-    private boolean hasLoop() {
-        boolean flag = false;
-        for (Path dependency : adjacencyList.keySet()) {
-            for (Node node : adjacencyList.get(dependency)) {
-                flag = flag || dfs(dependency, node.getPath());
-            }
-        }
-
-        return flag;
-    }
-
-    private boolean dfs(Path start, Path current) {
-        if (current == start) {
-            return true;
-        }
-        boolean flag = false;
-        for (Node node : adjacencyList.get(current)) {
-            flag = flag || dfs(start, node.getPath());
-        }
-
-        return flag;
     }
 
     public ArrayList<Path> getFiles() {
